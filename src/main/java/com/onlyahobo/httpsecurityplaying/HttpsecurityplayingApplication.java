ea87a9e103e79dc0c897fc04fcbf2040766f62b9
m11skowr <michal.skowronski@assecobs.pl>
2019-07-27 14:38:42 +0200

    Initial commit

---
+++ src/main/java/com/onlyahobo/httpsecurityplaying/HttpsecurityplayingApplication.java
@@ -0,0 +1,13 @@
+package com.onlyahobo.httpsecurityplaying;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpsecurityplayingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpsecurityplayingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onlyahobo/httpsecurityplaying/HttpsecurityplayingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.onlyahobo.httpsecurityplaying;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HttpsecurityplayingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
